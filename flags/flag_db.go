a36fc013b72a1ff6668dcd1cbb8161f3a6a3746d
LiangNing <liangning2277@gmail.com>
2025-01-10 23:12:27 +0800

    feat-007-所有表结构数据迁移

---
+++ flags/flag_db.go
@@ -0,0 +1,32 @@
+package flags
+
+import (
+	"github.com/LiangNing7/BlogX/global"
+	"github.com/LiangNing7/BlogX/models"
+	"github.com/sirupsen/logrus"
+)
+
+func FlagDB() {
+	err := global.DB.AutoMigrate(
+		&models.UserModel{},
+		&models.UserConfModel{},
+		&models.ArticleModel{},
+		&models.CategoryModel{},
+		&models.ArticleDiggModel{},
+		&models.CollectModel{},
+		&models.UserArticleCollectModel{},
+		&models.UserTopArticleModel{},
+		&models.ImageModel{},
+		&models.UserArticleLookHistoryModel{},
+		&models.CommentModel{},
+		&models.BannerModel{},
+		&models.LogModel{},
+		&models.UserLoginModel{},
+		&models.GlobalNotificationModel{},
+	)
+	if err != nil {
+		logrus.Errorf("数据库迁移失败 %s\n", err)
+		return
+	}
+	logrus.Infof("数据库迁移成功!\n")
+}
