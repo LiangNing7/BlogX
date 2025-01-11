bcb2476384d69f89f65402fcade86899af001fc8
LiangNing <liangning2277@gmail.com>
2025-01-11 23:53:26 +0800

    feat-011-操作日志-获取响应体

--- middleware/log_middleware.go
+++ middleware/log_middleware.go
@@ -9,6 +9,18 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// ResponseWriter 自定义的 ResponseWriter，Body用来捕获响应数据
+type ResponseWriter struct {
+	gin.ResponseWriter
+	Body []byte
+}
+
+// Write 重写 Write 方法，捕获响应内容
+func (w *ResponseWriter) Write(data []byte) (int, error) {
+	w.Body = append(w.Body, data...)
+	return w.ResponseWriter.Write(data)
+}
+
 func LogMiddleware(c *gin.Context) {
 	// 请求中间件
 	// 读原始 body
@@ -20,7 +32,11 @@ func LogMiddleware(c *gin.Context) {
 
 	// 将读取到的请求体重新赋值给 c.Request.Body
 	c.Request.Body = io.NopCloser(bytes.NewBuffer(byteData))
-	
+	res := &ResponseWriter{
+		ResponseWriter: c.Writer,
+	}
+	c.Writer = res
 	c.Next()
 	// 响应中间件
+	fmt.Println("response: ", string(res.Body))
 }
