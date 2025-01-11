7c488e9bd85d1d72ed518e6b2e2212c46f7cb801
LiangNing <liangning2277@gmail.com>
2025-01-11 22:39:47 +0800

    fix-001-c.Request.Body阅后即焚问题

--- api/site_api/enter.go
+++ api/site_api/enter.go
@@ -1,6 +1,7 @@
 package site_api
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 
@@ -27,19 +28,22 @@ type SiteUpdateRequest struct {
 func (SiteApi) SiteUpdateView(c *gin.Context) {
 	log := log_service.NewActionLogByGin(c)
 
-	// 通过绑定获取一次
-	var cr SiteUpdateRequest
-	err := c.ShouldBindJSON(&cr)
-	if err != nil {
-		logrus.Errorf(err.Error())
-	}
-	
 	// 读原始 body
 	byteData, err := io.ReadAll(c.Request.Body)
 	if err != nil {
 		logrus.Errorf(err.Error())
 	}
 	fmt.Println("body: ", string(byteData))
+
+	// 将读取到的请求体重新赋值给 c.Request.Body
+	c.Request.Body = io.NopCloser(bytes.NewBuffer(byteData))
+
+	// 通过绑定获取一次
+	var cr SiteUpdateRequest
+	err = c.ShouldBindJSON(&cr)
+	if err != nil {
+		logrus.Errorf(err.Error())
+	}
 
 	log.Save()
 	c.JSON(200, gin.H{"code": 0, "msg": "站点信息"})
