7c553c2a12e374111dced13ed73db0744fe56537
LiangNing <liangning2277@gmail.com>
2025-01-25 23:36:03 +0800

    feat-032-ES操作

---
+++ testdata/10-ES.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/LiangNing7/BlogX/core"
+	"github.com/LiangNing7/BlogX/flags"
+	"github.com/LiangNing7/BlogX/global"
+	"github.com/LiangNing7/BlogX/models"
+	"github.com/olivere/elastic/v7"
+)
+
+func create() {
+	var article = models.ArticleModel{
+		Model: models.Model{
+			ID: 1,
+		},
+		Title:   "LiangNing's Blog",
+		Content: "这是内容",
+		UserID:  1,
+		Status:  1,
+	}
+	indexResponse, err := global.ESClient.Index().Index(article.Index()).BodyJson(article).Do(context.Background())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%#v\n", indexResponse)
+}
+func list() {
+	limit := 2
+	page := 1
+	from := (page - 1) * limit
+	query := elastic.NewBoolQuery()
+	res, err := global.ESClient.Search(models.ArticleModel{}.Index()).Query(query).From(from).Size(limit).Do(context.Background())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	count := res.Hits.TotalHits.Value // 总数
+	fmt.Println(count)
+	for _, hit := range res.Hits.Hits {
+		fmt.Println(string(hit.Source))
+	}
+}
+func DocDelete() {
+	deleteResponse, err := global.ESClient.Delete().
+		Index(models.ArticleModel{}.Index()).Id("V9kUnpQBujP7135xix7G").Refresh("true").Do(context.Background())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(deleteResponse)
+}
+func update() {
+	updateResponse, err := global.ESClient.Update().Index(models.ArticleModel{}.Index()).Refresh("true").
+		Id("T14oB5MBjX_XsWH00KRt").
+		Doc(map[string]any{
+			"content": "枫枫1111",
+		}).Do(context.Background())
+	fmt.Println(updateResponse, err)
+}
+func main() {
+	flags.Parse()
+	global.Config = core.ReadConf()
+	core.InitLogrus()
+	global.ESClient = core.EsConnect()
+	// create()
+	// list()
+	DocDelete()
+	// update()
+}
