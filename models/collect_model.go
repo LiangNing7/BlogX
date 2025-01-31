a900c2b1a4c5e678beb87137404e7c15d71ae62b
LiangNing <liangning2277@gmail.com>
2025-01-31 17:38:01 +0800

    feat-042-文章收藏

--- models/collect_model.go
+++ models/collect_model.go
@@ -7,5 +7,6 @@ type CollectModel struct {
 	Cover        string    `gorm:"size:256" json:"cover"`
 	ArticleCount int       `json:"articleCount"`
 	UserID       uint      `json:"userID"`
+	IsDefault    bool      `json:"isDefault"` // 是否是默认收藏夹
 	UserModel    UserModel `gorm:"foreignKey:UserID" json:"-"`
 }
