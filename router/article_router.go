1001bc004256889b7a618a7f9dc58db1e89a2dd6
LiangNing <liangning2277@gmail.com>
2025-01-31 17:13:40 +0800

    feat-041-文章点赞

--- router/article_router.go
+++ router/article_router.go
@@ -14,4 +14,6 @@ func ArticleRouter(r *gin.RouterGroup) {
 	r.PUT("article", middleware.AuthMiddleware, middleware.BindJsonMiddleware[article_api.ArticleUpdateRequest], app.ArticleUpdateView)
 	r.GET("article", middleware.BindQueryMiddleware[article_api.ArticleListRequest], app.ArticleListView)
 	r.GET("article/:id", middleware.BindUriMiddleware[models.IDRequest], app.ArticleDetailView)
+	r.POST("article/examine", middleware.AdminMiddleware, middleware.BindJsonMiddleware[article_api.ArticleExamineRequest], app.ArticleExamineView)
+	r.GET("article/digg/:id", middleware.AuthMiddleware, middleware.BindUriMiddleware[models.IDRequest], app.ArticleDiggView)
 }
