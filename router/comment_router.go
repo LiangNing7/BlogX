477b442a0103285dc5f3fb008eb0f2f7009acdaf
LiangNing <liangning2277@gmail.com>
2025-02-02 00:44:12 +0800

    feat-059-评论列表

--- router/comment_router.go
+++ router/comment_router.go
@@ -12,4 +12,5 @@ func CommentRouter(r *gin.RouterGroup) {
 	app := api.App.CommentApi
 	r.POST("comment", middleware.AuthMiddleware, middleware.BindJsonMiddleware[comment_api.CommentCreateRequest], app.CommentCreateView)
 	r.GET("comment/tree/:id", middleware.BindUriMiddleware[models.IDRequest], app.CommentTreeView)
+	r.GET("comment", middleware.AuthMiddleware, middleware.BindQueryMiddleware[comment_api.CommentListRequest], app.CommentListView)
 }
