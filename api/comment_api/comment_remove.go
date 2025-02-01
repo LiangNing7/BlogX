0261c8f3f054b6a8e120235dafb80b032ea51810
LiangNing <liangning2277@gmail.com>
2025-02-02 02:10:29 +0800

    feat-065-评论删除

---
+++ api/comment_api/comment_remove.go
@@ -0,0 +1,46 @@
+package comment_api
+
+import (
+	"fmt"
+
+	"github.com/LiangNing7/BlogX/common/res"
+	"github.com/LiangNing7/BlogX/global"
+	"github.com/LiangNing7/BlogX/middleware"
+	"github.com/LiangNing7/BlogX/models"
+	"github.com/LiangNing7/BlogX/models/enum"
+	"github.com/LiangNing7/BlogX/service/comment_service"
+	"github.com/LiangNing7/BlogX/service/redis_service/redis_comment"
+	"github.com/LiangNing7/BlogX/utils/jwts"
+	"github.com/gin-gonic/gin"
+)
+
+func (CommentApi) CommentRemoveApi(c *gin.Context) {
+	var cr = middleware.GetBind[models.IDRequest](c)
+	claims := jwts.GetClaims(c)
+	var model models.CommentModel
+	err := global.DB.Preload("ArticleModel").Take(&model, cr.ID).Error
+	if err != nil {
+		res.FailWithMsg("评论不存在", c)
+		return
+	}
+	if claims.Role != enum.AdminRole {
+		// 普通用户只能删自己发的评论，或者自己发的文章的评论
+		if !(model.UserID == claims.UserID || model.ArticleModel.UserID == claims.UserID) {
+			res.FailWithMsg("权限错误", c)
+			return
+		}
+	}
+	// 找所有的子评论，还要找所有的父评论
+	subList := comment_service.GetCommentOneDimensional(model.ID)
+	if model.ParentID != nil {
+		// 有父评论
+		parentList := comment_service.GetParents(*model.ParentID)
+		for _, commentModel := range parentList {
+			redis_comment.SetCacheApply(commentModel.ID, -len(subList))
+		}
+	}
+	// 删评论
+	global.DB.Delete(&subList)
+	msg := fmt.Sprintf("删除成功，共删除评论%d条", len(subList))
+	res.OkWithMsg(msg, c)
+}
