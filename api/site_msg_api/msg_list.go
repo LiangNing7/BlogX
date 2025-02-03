0f753b9473d18bf47fd8bc26531abb9c755ecb2a
LiangNing <liangning2277@gmail.com>
2025-02-03 15:34:43 +0800

    feat-078-站内信列表

---
+++ api/site_msg_api/msg_list.go
@@ -0,0 +1,38 @@
+package site_msg_api
+
+import (
+	"github.com/LiangNing7/BlogX/common"
+	"github.com/LiangNing7/BlogX/common/res"
+	"github.com/LiangNing7/BlogX/global"
+	"github.com/LiangNing7/BlogX/middleware"
+	"github.com/LiangNing7/BlogX/models"
+	"github.com/LiangNing7/BlogX/models/enum/message_type_enum"
+	"github.com/LiangNing7/BlogX/utils/jwts"
+	"github.com/gin-gonic/gin"
+)
+
+type SiteMsgListRequest struct {
+	common.PageInfo
+	T int8 `form:"t" binding:"required,oneof=1 2 3"` // 1评论和回复 2赞和收藏 3 系统
+}
+
+func (SiteMsgApi) SiteMsgListView(c *gin.Context) {
+	cr := middleware.GetBind[SiteMsgListRequest](c)
+	var typeList []message_type_enum.Type
+	switch cr.T {
+	case 1:
+		typeList = append(typeList, message_type_enum.CommentType, message_type_enum.ApplyType)
+	case 2:
+		typeList = append(typeList, message_type_enum.DiggArticleType, message_type_enum.DiggCommentType, message_type_enum.CollectArticleType)
+	case 3:
+		typeList = append(typeList, message_type_enum.SystemType)
+	}
+	claims := jwts.GetClaims(c)
+	list, count, _ := common.ListQuery(models.MessageModel{
+		RevUserID: claims.UserID,
+	}, common.Options{
+		PageInfo: cr.PageInfo,
+		Where:    global.DB.Where("type in ?", typeList),
+	})
+	res.OkWithList(list, count, c)
+}
