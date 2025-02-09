67bbd8a9b975af53a85f1224d782960d05a9a8bc
LiangNing <liangning2277@gmail.com>
2025-02-09 15:49:31 +0800

    feat-096-私信-用户删除消息

--- router/chat_router.go
+++ router/chat_router.go
@@ -4,6 +4,7 @@ import (
 	"github.com/LiangNing7/BlogX/api"
 	"github.com/LiangNing7/BlogX/api/chat_api"
 	"github.com/LiangNing7/BlogX/middleware"
+	"github.com/LiangNing7/BlogX/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,4 +12,5 @@ func ChatRouter(r *gin.RouterGroup) {
 	app := api.App.ChatApi
 	r.GET("chat", middleware.AuthMiddleware, middleware.BindQueryMiddleware[chat_api.ChatListRequest], app.ChatListView)
 	r.GET("chat/session", middleware.AuthMiddleware, middleware.BindQueryMiddleware[chat_api.SessionListRequest], app.SessionListView)
+	r.DELETE("chat/:id", middleware.AuthMiddleware, middleware.BindUriMiddleware[models.IDRequest], app.UserChatDeleteView)
 }
