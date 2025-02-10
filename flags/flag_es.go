87551fd94ca863e4b5b84b00924197b8bc7b5a4d
LiangNing <liangning2277@gmail.com>
2025-02-10 18:33:09 +0800

    feat-109-全文记录同步

--- flags/flag_es.go
+++ flags/flag_es.go
@@ -1,11 +1,19 @@
 package flags
 
 import (
+	"github.com/LiangNing7/BlogX/global"
 	"github.com/LiangNing7/BlogX/models"
 	"github.com/LiangNing7/BlogX/service/es_service"
+	"github.com/sirupsen/logrus"
 )
 
 func EsIndex() {
+	if global.ESClient == nil {
+		logrus.Warnf("未开启es连接")
+		return
+	}
 	article := models.ArticleModel{}
 	es_service.CreateIndexV2(article.Index(), article.Mapping())
+	text := models.TextModel{}
+	es_service.CreateIndexV2(text.Index(), text.Mapping())
 }
