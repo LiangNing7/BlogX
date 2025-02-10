c8190299861360a6b6d2f1f76ed238f532912b3e
LiangNing <liangning2277@gmail.com>
2025-02-11 00:03:01 +0800

    feat-115-AI-AI服务降级

--- api/ai_api/article_ai.go
+++ api/ai_api/article_ai.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"strings"
 
+	"github.com/LiangNing7/BlogX/common"
 	"github.com/LiangNing7/BlogX/common/res"
 	"github.com/LiangNing7/BlogX/global"
 	"github.com/LiangNing7/BlogX/middleware"
@@ -25,33 +26,47 @@ func (AiApi) ArticleAiView(c *gin.Context) {
 		res.SSEFail("站点未启用ai功能", c)
 		return
 	}
-	// 查这个内容关联的文章列表
-	query := elastic.NewBoolQuery()
-	query.Should(
-		elastic.NewMatchQuery("title", cr.Content),
-		elastic.NewMatchQuery("abstract", cr.Content),
-		elastic.NewMatchQuery("content", cr.Content),
-	)
-	// 只能查发布的文章
-	query.Must(elastic.NewTermQuery("status", 3))
-	result, err := global.ESClient.
-		Search(models.ArticleModel{}.Index()).
-		Query(query).
-		From(1).
-		Size(10).
-		Do(context.Background())
-	if err != nil {
-		source, _ := query.Source()
-		byteData, _ := json.Marshal(source)
-		logrus.Errorf("查询失败 %s \n %s", err, string(byteData))
-		res.SSEFail("查询失败", c)
-		return
+	var content string
+	if global.ESClient == nil {
+		// 服务降级
+		list, _, _ := common.ListQuery(models.ArticleModel{}, common.Options{
+			Likes: []string{"title", "abstract"},
+			PageInfo: common.PageInfo{
+				Page:  1,
+				Limit: 10,
+			},
+		})
+		byteData, _ := json.Marshal(list)
+		content = string(byteData)
+	} else {
+		// 查这个内容关联的文章列表
+		query := elastic.NewBoolQuery()
+		query.Should(
+			elastic.NewMatchQuery("title", cr.Content),
+			elastic.NewMatchQuery("abstract", cr.Content),
+			elastic.NewMatchQuery("content", cr.Content),
+		)
+		// 只能查发布的文章
+		query.Must(elastic.NewTermQuery("status", 3))
+		result, err := global.ESClient.
+			Search(models.ArticleModel{}.Index()).
+			Query(query).
+			From(1).
+			Size(10).
+			Do(context.Background())
+		if err != nil {
+			source, _ := query.Source()
+			byteData, _ := json.Marshal(source)
+			logrus.Errorf("查询失败 %s \n %s", err, string(byteData))
+			res.SSEFail("查询失败", c)
+			return
+		}
+		var list []string
+		for _, hit := range result.Hits.Hits {
+			list = append(list, string(hit.Source))
+		}
+		content = "[" + strings.Join(list, ",") + "]"
 	}
-	var list []string
-	for _, hit := range result.Hits.Hits {
-		list = append(list, string(hit.Source))
-	}
-	content := "[" + strings.Join(list, ",") + "]"
 	msgChan, err := ai_service.ChatStream(cr.Content, content)
 	if err != nil {
 		res.SSEFail("ai分析失败", c)
--- core/init_es.go
+++ core/init_es.go
@@ -9,6 +9,7 @@ import (
 func EsConnect() *elastic.Client {
 	es := global.Config.ES
 	if !es.Enable || es.Addr == "" {
+		logrus.Infof("未启用es连接")
 		return nil
 	}
 	client, err := elastic.NewClient(
