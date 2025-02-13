56beb9ff7129d6a29be0004c8d1567049720fc8c
LiangNing <liangning2277@gmail.com>
2025-02-14 01:07:12 +0800

    feat-123-数据统计-用户统计

--- router/data_router.go
+++ router/data_router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/LiangNing7/BlogX/api"
+	"github.com/LiangNing7/BlogX/api/data_api"
 	"github.com/LiangNing7/BlogX/middleware"
 	"github.com/gin-gonic/gin"
 )
@@ -10,4 +11,6 @@ func DataRouter(r *gin.RouterGroup) {
 	app := api.App.DataApi
 	r.GET("data/sum", middleware.AdminMiddleware, app.SumView)
 	r.GET("data/article", middleware.AdminMiddleware, app.ArticleDataView)
+	r.GET("data/register_user", middleware.AdminMiddleware, app.RegisterUserDataView)
+	r.GET("data/growth", middleware.AdminMiddleware, middleware.BindQueryMiddleware[data_api.GrowthDataRequest], app.GrowthDataView)
 }
