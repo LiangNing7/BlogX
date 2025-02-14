bf7a07373e3dd2ef8675322a028980d514e32612
LiangNing <liangning2277@gmail.com>
2025-02-14 17:48:52 +0800

    feat-148-联调-消息列表添加好友关系字段

--- api/site_msg_api/msg_list.go
+++ api/site_msg_api/msg_list.go
@@ -7,6 +7,8 @@ import (
 	"github.com/LiangNing7/BlogX/middleware"
 	"github.com/LiangNing7/BlogX/models"
 	"github.com/LiangNing7/BlogX/models/enum/message_type_enum"
+	"github.com/LiangNing7/BlogX/models/enum/relationship_enum"
+	"github.com/LiangNing7/BlogX/service/focus_service"
 	"github.com/LiangNing7/BlogX/utils/jwts"
 	"github.com/gin-gonic/gin"
 )
@@ -14,6 +16,11 @@ import (
 type SiteMsgListRequest struct {
 	common.PageInfo
 	T int8 `form:"t" binding:"required,oneof=1 2 3"` // 1评论和回复 2赞和收藏 3 系统
+}
+
+type SiteMsgListResponse struct {
+	models.MessageModel
+	Relation relationship_enum.Relation `json:"relation"`
 }
 
 func (SiteMsgApi) SiteMsgListView(c *gin.Context) {
@@ -28,11 +35,29 @@ func (SiteMsgApi) SiteMsgListView(c *gin.Context) {
 		typeList = append(typeList, message_type_enum.SystemType)
 	}
 	claims := jwts.GetClaims(c)
-	list, count, _ := common.ListQuery(models.MessageModel{
+	_list, count, _ := common.ListQuery(models.MessageModel{
 		RevUserID: claims.UserID,
 	}, common.Options{
 		PageInfo: cr.PageInfo,
 		Where:    global.DB.Where("type in ?", typeList),
 	})
+
+	var userIDList []uint
+	for _, model := range _list {
+		if model.ActionUserID != 0 {
+			userIDList = append(userIDList, model.ActionUserID)
+		}
+	}
+	var m = map[uint]relationship_enum.Relation{}
+	if len(userIDList) > 0 {
+		m = focus_service.CalcUserPatchRelationship(claims.UserID, userIDList)
+	}
+	var list = make([]SiteMsgListResponse, 0)
+	for _, model := range _list {
+		list = append(list, SiteMsgListResponse{
+			MessageModel: model,
+			Relation:     m[model.ActionUserID],
+		})
+	}
 	res.OkWithList(list, count, c)
 }
